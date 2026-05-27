0204a6b1dced7c4b3c2e3805e4254079719df365
maxim <maxim.skalenko@gmail.com>
2026-05-27 23:10:16 +0200

    implemented selection of the best

---
+++ src/selection.py
@@ -0,0 +1,5 @@
+from representation import tour_length
+
+def selection(population, cities, mu):
+    sorted_pop = sorted(population, key=lambda tour: tour_length(tour, cities))
+    return sorted_pop[:mu]
